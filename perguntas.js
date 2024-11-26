dd2ceb9f4f29c1c973f7d0365f6101d3b8dba2f4
xuxuzinhooo <rebecaoliveira95685@gmail.com>
2024-11-26 12:34:29 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'Com quantos anos a rainha Elizabet faleceu?',
+    'com 101 anos'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital dos Estados Unidos?',
+    'A capital dos Estados Unidos é Washiton D.C'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma resenha?',
+    ' um texto curto que tem como objetivo transmitir ao leitor a ideia contida em um livro, um artigo ou um filme'
+)
+
+criaCartao(
+    'Lingua francêsa',
+    'Como se diz oi em francês?',
+    'Oi em francês é Hé'
+)
